88811831dbb3d6cf2fa7d573645d930c9581e875
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2025-03-06 22:03:20 -0600

    Cambios en el borrar y destacar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   nuevaTareaMinutos: number | null = null;
   tareasSeleccionadas: Set<number> = new Set();
   ordenAscendente: boolean = true;
-  modalVisible: boolean = false;
+  modalVisible: boolean = false; // nos indica el estado del modal
 
   constructor(public service: AppService) {}
 
@@ -24,7 +24,7 @@ export class AppComponent {
   async obtenerTareas() {
     this.tareas = await this.service.obtenerTareas();
   }
-
+  // funcion para agregar una nueva tarea
   agregarTarea() {
     if (this.nuevaTareaTitulo && this.nuevaTareaMinutos !== null) {
       const nuevaTarea = new Tarea(
@@ -37,10 +37,11 @@ export class AppComponent {
       this.nuevaTareaMinutos = null;
 	  this.cerrarModal();
     } else {
-	  alert('Por favor, complete los campos de la nueva tarea.');
+	  alert('Por favor, complete los campos de la nueva tarea.'); // alerta si no se completan los campos. 
 	}
   }
 
+  // Funciones para seleccionar, eliminar y destacar tareas
   toggleSeleccion(id: number) {
     if (this.tareasSeleccionadas.has(id)) {
       this.tareasSeleccionadas.delete(id);
@@ -48,30 +49,29 @@ export class AppComponent {
       this.tareasSeleccionadas.add(id);
     }
   }
-
+  // funcion para eliminar tareas
   eliminarTareas() {
     this.tareas = this.tareas.filter(t => !this.tareasSeleccionadas.has(t.id));
-    this.tareasSeleccionadas.clear();
+    this.tareasSeleccionadas.clear(); // limpiamos el set de tareas seleccionadas
   }
-
-  ordenarPor(columna: 'titulo' | 'minutos') {
+  // funcion para ordenar las tareas por titulo o minutos
+  ordenarPor(columna: 'titulo' | 'minutos') { // definimos el tipo de columna
     this.ordenAscendente = !this.ordenAscendente;
     this.tareas.sort((a, b) => this.ordenAscendente 
-      ? (a[columna] > b[columna] ? 1 : -1)
-      : (a[columna] < b[columna] ? 1 : -1));
+      ? (a[columna] > b[columna] ? 1 : -1) // si es ascendente
+      : (a[columna] < b[columna] ? 1 : -1)); // si es descendente
   }
-
+  // funcion para destacar tareas
   destacarTareas() {
     this.tareas.forEach(t => {
       if (this.tareasSeleccionadas.has(t.id)) {
-        t.destacada = !t.destacada;
+        t.destacada = !t.destacada; // cambiamos el estado de destacada
       }
     });
-	this.tareasSeleccionadas.clear();
   }
-
+  // funcion para ordenar las tareas de forma aleatoria
   ordenarAleatorio() {
-    this.tareas.sort(() => Math.random() - 0.5);
+    this.tareas.sort(() => Math.random() - 0.5); // ordenamos de forma aleatoria
   }
   // Funciones para abrir y cerrar el modal
   abrirModal() {
